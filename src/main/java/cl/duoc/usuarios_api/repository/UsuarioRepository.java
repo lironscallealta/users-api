0d70fee8da4f177b51d6cef18c713c484ad666d5
lironscallealta <lironscallealta@gmail.com>
2026-05-13 19:39:38 -0400

    feat

--- src/main/java/cl/duoc/usuarios_api/repository/UsuarioRepository.java
+++ src/main/java/cl/duoc/usuarios_api/repository/UsuarioRepository.java
@@ -8,7 +8,5 @@ package cl.duoc.usuarios_api.repository;
 
 import cl.duoc.usuarios_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
