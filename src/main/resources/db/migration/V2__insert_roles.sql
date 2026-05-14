5617735201903152b3326e02f7fbaa0e3ca40e2a
lironscallealta <lironscallealta@gmail.com>
2026-05-14 13:30:10 -0400

    feat: add initial roles and seed sample users to the database migration script

--- src/main/resources/db/migration/V2__insert_roles.sql
+++ src/main/resources/db/migration/V2__insert_roles.sql
@@ -8,8 +8,8 @@ INSERT INTO roles (nombre_rol, descripcion) VALUES
 
 -- Usuarios de ejemplo (con cada una de las opciones de rol)
 INSERT INTO usuarios (nombre, apellido, rut, dv, email, telefono_celular, fecha_nacimiento_usuario, rol_id, activo, fecha_creacion_usuario) VALUES
-('Admin', 'Ejemplo', '11111111', '1', 'admin@ejemplo.com', '+56911111111', '1980-01-01', 1, TRUE, CURRENT_TIMESTAMP),
-('Gerente', 'Ejemplo', '22222222', '2', 'gerente@ejemplo.com', '+56922222222', '1985-05-15', 2, TRUE, CURRENT_TIMESTAMP),
-('Empleado', 'Ejemplo', '33333333', '3', 'empleado@ejemplo.com', '+56933333333', '1990-10-20', 3, TRUE, CURRENT_TIMESTAMP),
-('Veterinario', 'Ejemplo', '44444444', '4', 'veterinario@ejemplo.com', '+56944444444', '1988-12-10', 4, TRUE, CURRENT_TIMESTAMP),
-('Cliente', 'Ejemplo', '55555555', '5', 'cliente@ejemplo.com', '+56955555555', '1995-03-25', 5, TRUE, CURRENT_TIMESTAMP);
+('Carlos', 'Perez', '15342123', '1', 'carlos.admin@ejemplo.com', '+56912345678', '1980-01-01', 1, TRUE, CURRENT_TIMESTAMP),
+('Maria', 'Gonzalez', '16453234', 'K', 'maria.gerente@ejemplo.com', '+56987654321', '1985-05-15', 2, TRUE, CURRENT_TIMESTAMP),
+('Roberto', 'Diaz', '17564345', '9', 'roberto.empleado@ejemplo.com', '+56911223344', '1990-10-20', 3, TRUE, CURRENT_TIMESTAMP),
+('Camila', 'Silva', '18675456', '7', 'camila.veterinario@ejemplo.com', '+56955667788', '1988-12-10', 4, TRUE, CURRENT_TIMESTAMP),
+('Diego', 'Rojas', '19786567', '5', 'diego.cliente@ejemplo.com', '+56999887766', '1995-03-25', 5, TRUE, CURRENT_TIMESTAMP);
