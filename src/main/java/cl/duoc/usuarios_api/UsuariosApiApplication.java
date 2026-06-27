63322b699d7ea6d8ac4c96da4950501ca2fac964
polirritmico <ebray187@gmail.com>
2026-06-27 15:39:04 -0400

    feat: add eureka discovery & update .env.example

--- src/main/java/cl/duoc/usuarios_api/UsuariosApiApplication.java
+++ src/main/java/cl/duoc/usuarios_api/UsuariosApiApplication.java
@@ -9,8 +9,10 @@ package cl.duoc.usuarios_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class UsuariosApiApplication {
 
     public static void main(String[] args) {
